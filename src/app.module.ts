9604fcdf66ff758630ae11f2142dd832a361a572
Daniel Lwetabe <dannylwe11@gmail.com>
2023-09-25 04:12:31 +0300

    setup database complete

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,22 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { DatabaseModule } from './database/database.module';
 import { ConfigModule } from '@nestjs/config';
+import { SequelizeModule } from '@nestjs/sequelize';
+import * as dotenv from 'dotenv';
+dotenv.config();
 
 @Module({
-  imports: [DatabaseModule, ConfigModule.forRoot({ isGlobal: true })],
+  imports: [SequelizeModule.forRoot({
+    dialect: 'postgres',
+    host: 'localhost',
+    port: 54320,
+    username: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME_DEVELOPMENT,
+    autoLoadModels: true,
+    synchronize: true
+  }), ConfigModule.forRoot({ isGlobal: true })],
   controllers: [AppController],
   providers: [AppService],
 })
